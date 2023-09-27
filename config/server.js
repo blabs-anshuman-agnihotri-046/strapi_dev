f292bccabc9d19ad16b6db40f48f30080f0cb4fc
blabs-anshuman-agnihotri-046 <126248337+blabs-anshuman-agnihotri-046@users.noreply.github.com>
2023-09-28 03:05:29 +0530

    Update server.js

--- config/server.js
+++ config/server.js
@@ -7,4 +7,10 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  middleware: {
+    // Add the autoReload middleware
+    autoReload: {
+      enabled: true,
+    },
+  },
 });
