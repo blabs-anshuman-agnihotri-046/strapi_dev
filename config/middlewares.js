d4909874d6436171a3ff8637f70300a11a204a79
anshuman <anshuman.agnihotri@benthonlabs.com>
2023-10-11 17:13:49 +0530

    added container url

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,7 @@
 module.exports = [
   'strapi::errors',
   {
-    name: "strapi::security",
+    name: 'strapi::security',
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
@@ -12,23 +12,15 @@ module.exports = [
             "data:",
             "blob:",
             "dl.airtable.com",
-            "https://market-assets.strapi.io",
-            /**
-             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
-             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
-             */
-            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+            "market-assets.strapi.io",
+            `csstrapistorage-secondary.z13.web.core.windows.net`,
           ],
           "media-src": [
             "'self'",
             "data:",
             "blob:",
             "dl.airtable.com",
-            /**
-             * Note: If using a STORAGE_URL replace `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net` w/ process.env.STORAGE_URL
-             * If using a CDN URL make sure to include that url in the CSP headers process.env.STORAGE_CDN_URL
-             */
-            `https://${process.env.STORAGE_ACCOUNT}.blob.core.windows.net`,
+            `csstrapistorage-secondary.z13.web.core.windows.net`,
 
           ],
           upgradeInsecureRequests: null,
--- config/plugins.js
+++ config/plugins.js
@@ -5,7 +5,8 @@ module.exports = ({ env }) => ({
         providerOptions: {
           account: env("STORAGE_ACCOUNT"),
           accountKey: env("STORAGE_ACCOUNT_KEY"),
-   //       serviceBaseURL: env("STORAGE_URL"), // optional
+ //         serviceBaseURL: env("STORAGE_URL"), // optional
+          serviceBaseURL: 'https://csstrapistorage.blob.core.windows.net/strapicontainer',
           containerName: env("STORAGE_CONTAINER_NAME"),
 //          defaultPath: "assets",
 //          cdnBaseURL: env("STORAGE_CDN_URL"), // optional
